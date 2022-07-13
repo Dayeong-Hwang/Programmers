0c1284e11099cb2eff677264de02c462eb48d249
hdy <d-yeong@naver.com>
2022-07-13 20:42:14 +0900

    레벨3  SQL 완료

--- programmers/src/level3/MySQL.java
+++ programmers/src/level3/MySQL.java
@@ -51,7 +51,21 @@ public class MySQL {
 	
 	
 	//오랜 기간 보호한 동물(1)
+//	SELECT A.NAME, A.DATETIME
+//	FROM ANIMAL_INS A LEFT JOIN ANIMAL_OUTS B
+//	ON A.ANIMAL_ID = B.ANIMAL_ID
+//	WHERE B.ANIMAL_ID IS NULL
+//	ORDER BY A.DATETIME ASC LIMIT 3;
 
+// 참고 블로그 : https://it-jin-developer.tistory.com/42
+	
+	
+	//오랜 기간 보호한 동물(2)
+	//DATEDIFF 함수도 있음
+//	SELECT A.ANIMAL_ID, A.NAME
+//	FROM ANIMAL_INS A LEFT JOIN ANIMAL_OUTS B
+//	ON A.ANIMAL_ID = B.ANIMAL_ID
+//	ORDER BY B.DATETIME - A.DATETIME DESC LIMIT 2;
 	
 	
 }
